de9b87c789fe3488a72f1b317d9cb7b6c2aea5e2
Ben Davis <ben@davisben.com>
2017-09-20 21:58:36 -0400

    File loading

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,9 +1,35 @@
 <template lang="pug">
-  header SMS Backup Viewer
+  header
+    h1 SMS Backup Viewer
+    input(type='file', accept='text/xml', @change='onFileChange')
 </template>
 
 <script>
+import Parser from 'xml2js'
+
 export default {
-  name: 'page-header'
+  name: 'page-header',
+  store: ['contacts', 'messages'],
+  methods: {
+    onFileChange (e) {
+      var files = e.target.files || e.dataTransfer.files
+      if (!files.length) {
+        return
+      }
+
+      var reader = new FileReader()
+      reader.onload = function (e) {
+        var xml = e.target.result
+
+        Parser.parseString(xml, function (err, result) {
+          if (err) {
+            console.log(err.stack)
+          }
+          console.dir(result)
+        })
+      }
+      reader.readAsText(files[0])
+    }
+  }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
+import VueStash from 'vue-stash'
 import App from './App'
+
+Vue.use(VueStash)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  data: {
+    store: {
+      contacts: '',
+      messages: ''
+    }
+  },
   render: h => h(App)
 })
